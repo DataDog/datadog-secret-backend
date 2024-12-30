153ec15909b4a5da603adb25699a24047368c7bc
Baptiste Foy <baptiste.foy@datadoghq.com>
2024-12-30 12:11:06 +0100

    test(ssm): Add test for AWS SSM backend

--- backend/aws/ssm.go
+++ backend/aws/ssm.go
@@ -17,6 +17,20 @@ import (
 	"github.com/DataDog/datadog-secret-backend/secret"
 )
 
+// ssmClient is an interface that defines the methods we use from the ssm client
+// As the AWS SDK doesn't provide a real mock, we'll have to make our own that
+// matches this interface
+type ssmClient interface {
+	GetParametersByPath(ctx context.Context, params *ssm.GetParametersByPathInput, optFns ...func(*ssm.Options)) (*ssm.GetParametersByPathOutput, error)
+	GetParameters(ctx context.Context, params *ssm.GetParametersInput, optFns ...func(*ssm.Options)) (*ssm.GetParametersOutput, error)
+}
+
+// getClient is a variable that holds the function to create a new ssmClient
+// it will be overwritten in tests
+var getClient = func(cfg aws.Config) ssmClient {
+	return ssm.NewFromConfig(cfg)
+}
+
 // SSMParameterStoreBackendConfig is the configuration for a AWS SSM backend
 type SSMParameterStoreBackendConfig struct {
 	Session       SessionBackendConfig `mapstructure:"aws_session"`
@@ -52,7 +66,7 @@ func NewSSMParameterStoreBackend(backendID string, bc map[string]interface{}) (
 			Msg("failed to initialize aws session")
 		return nil, err
 	}
-	client := ssm.NewFromConfig(*cfg)
+	client := getClient(*cfg)
 
 	// GetParametersByPath
 	if backendConfig.ParameterPath != "" {
---
+++ backend/aws/ssm_test.go
@@ -0,0 +1,125 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the BSD 3-Clause License.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2024-present Datadog, Inc.
+// Copyright (c) 2021, RapDev.IO
+
+package aws
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/stretchr/testify/assert"
+)
+
+// ssmMockClient is the struct we'll use to mock the SSM client
+// for unit tests. E2E tests should be written with the real client.
+type ssmMockClient struct {
+	parameters map[string]interface{}
+}
+
+func (c *ssmMockClient) GetParametersByPath(ctx context.Context, params *ssm.GetParametersByPathInput, optFns ...func(*ssm.Options)) (*ssm.GetParametersByPathOutput, error) {
+	if params == nil || params.Path == nil {
+		return nil, nil
+	}
+	outParameters := []types.Parameter{}
+	for key, value := range c.parameters {
+		if strings.HasPrefix(key, *params.Path) {
+			outParameters = append(outParameters, types.Parameter{
+				Name:  aws.String(key),
+				Value: aws.String(value.(string)),
+			})
+		}
+	}
+
+	return &ssm.GetParametersByPathOutput{
+		Parameters: outParameters,
+	}, nil
+}
+
+func (c *ssmMockClient) GetParameters(ctx context.Context, params *ssm.GetParametersInput, optFns ...func(*ssm.Options)) (*ssm.GetParametersOutput, error) {
+	outParameters := []types.Parameter{}
+	for key, value := range c.parameters {
+		for _, name := range params.Names {
+			if key == name {
+				outParameters = append(outParameters, types.Parameter{
+					Name:  aws.String(key),
+					Value: aws.String(value.(string)),
+				})
+				break
+			}
+		}
+	}
+
+	return &ssm.GetParametersOutput{
+		Parameters: outParameters,
+	}, nil
+}
+
+func TestSSMParameterStoreBackend_Parameters(t *testing.T) {
+	mockClient := &ssmMockClient{
+		parameters: map[string]interface{}{
+			"/group1/key1":      "value1",
+			"/group1/nest/key2": "value2",
+		},
+	}
+	getClient = func(_ aws.Config) ssmClient {
+		return mockClient
+	}
+
+	ssmParameterStoreBackendParams := map[string]interface{}{
+		"backend_type": "ssmParameterStore",
+		"parameters":   []string{"/group1/key1", "/group1/nest/key2"},
+	}
+	ssmParameterStoreSecretsBackend, err := NewSSMParameterStoreBackend("ssmParameterStore-backend", ssmParameterStoreBackendParams)
+	assert.NoError(t, err)
+
+	secretOutput := ssmParameterStoreSecretsBackend.GetSecretOutput("/group1/key1")
+	assert.Equal(t, "value1", *secretOutput.Value)
+	assert.Nil(t, secretOutput.Error)
+
+	secretOutput = ssmParameterStoreSecretsBackend.GetSecretOutput("key_noexist")
+	assert.Nil(t, secretOutput.Value)
+	assert.Equal(t, "backend does not provide secret key", *secretOutput.Error)
+}
+
+func TestSSMParameterStoreBackend_ParametersByPath(t *testing.T) {
+	mockClient := &ssmMockClient{
+		parameters: map[string]interface{}{
+			"/group1/key1":      "value1",
+			"/group1/nest/key2": "value2",
+			"/group2/key3":      "value3",
+		},
+	}
+	getClient = func(_ aws.Config) ssmClient {
+		return mockClient
+	}
+
+	ssmParameterStoreBackendParams := map[string]interface{}{
+		"backend_type":   "ssmParameterStore",
+		"parameter_path": "/group1",
+	}
+	ssmParameterStoreSecretsBackend, err := NewSSMParameterStoreBackend("ssmParameterStore-backend", ssmParameterStoreBackendParams)
+	assert.NoError(t, err)
+
+	secretOutput := ssmParameterStoreSecretsBackend.GetSecretOutput("/group1/key1")
+	assert.Equal(t, "value1", *secretOutput.Value)
+	assert.Nil(t, secretOutput.Error)
+
+	secretOutput = ssmParameterStoreSecretsBackend.GetSecretOutput("/group1/nest/key2")
+	assert.Equal(t, "value2", *secretOutput.Value)
+	assert.Nil(t, secretOutput.Error)
+
+	secretOutput = ssmParameterStoreSecretsBackend.GetSecretOutput("/group1/key_noexist")
+	assert.Nil(t, secretOutput.Value)
+	assert.Equal(t, "backend does not provide secret key", *secretOutput.Error)
+
+	secretOutput = ssmParameterStoreSecretsBackend.GetSecretOutput("/group2/key3")
+	assert.Nil(t, secretOutput.Value)
+	assert.Equal(t, "backend does not provide secret key", *secretOutput.Error)
+}
