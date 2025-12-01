ad7e943f20aa46796d7b4603b0160ad314da8c26
s-alad <saad.naji@datadoghq.com>
2025-12-01 18:38:30 +0100

    add k8s and docker backend type to `backend.go`

--- backend/backend.go
+++ backend/backend.go
@@ -14,9 +14,11 @@ import (
 	"github.com/DataDog/datadog-secret-backend/backend/akeyless"
 	"github.com/DataDog/datadog-secret-backend/backend/aws"
 	"github.com/DataDog/datadog-secret-backend/backend/azure"
+	"github.com/DataDog/datadog-secret-backend/backend/docker"
 	"github.com/DataDog/datadog-secret-backend/backend/file"
 	"github.com/DataDog/datadog-secret-backend/backend/gcp"
 	"github.com/DataDog/datadog-secret-backend/backend/hashicorp"
+	"github.com/DataDog/datadog-secret-backend/backend/kubernetes"
 	"github.com/DataDog/datadog-secret-backend/secret"
 )
 
@@ -45,6 +47,10 @@ func Get(backendType string, backendConfig map[string]interface{}) Backend {
 		backend, err = file.NewYAMLBackend(backendConfig)
 	case "file.json":
 		backend, err = file.NewJSONBackend(backendConfig)
+	case "k8s.secrets":
+		backend, err = kubernetes.NewSecretsBackend(backendConfig)
+	case "docker.secrets":
+		backend, err = docker.NewSecretsBackend(backendConfig)
 	case "akeyless":
 		backend, err = akeyless.NewAkeylessBackend(backendConfig)
 	default:
